3bd56f30493cb970441145c57ea21f6cb5c18563
Ryan Kelley <kelleyr@duvalschools.org>
2021-09-18 21:52:23 -0400

    Create TopicOnePointThreeReview.java
    Start of review for Topic 1.3.

---
+++ topic1.3/TopicOnePointThreeReview.java
@@ -0,0 +1,47 @@
+/*
+
+      .o.       ooooooooo.          .oooooo.    .oooooo..o       .o.                  
+     .888.      `888   `Y88.       d8P'  `Y8b  d8P'    `Y8      .888.                 
+    .8"888.      888   .d88'      888          Y88bo.          .8"888.                
+   .8' `888.     888ooo88P'       888           `"Y8888o.     .8' `888.               
+  .88ooo8888.    888              888               `"Y88b   .88ooo8888.              
+ .8'     `888.   888              `88b    ooo  oo     .d8P  .8'     `888.             
+o88o     o8888o o888o              `Y8bood8P'  8""88888P'  o88o     o8888o            
+                                                                                                                                                                                                                                                                  
+ooooooooooooo   .oooooo.   ooooooooo.   ooooo   .oooooo.          .o        .oooo.    
+8'   888   `8  d8P'  `Y8b  `888   `Y88. `888'  d8P'  `Y8b       o888      .dP""Y88b   
+     888      888      888  888   .d88'  888  888                888            ]8P'  
+     888      888      888  888ooo88P'   888  888                888          <88b.   
+     888      888      888  888          888  888                888           `88b.  
+     888      `88b    d88'  888          888  `88b    ooo        888  .o. o.   .88P   
+    o888o      `Y8bood8P'  o888o        o888o  `Y8bood8P'       o888o Y8P `8bd88P'    
+                                                                                                                                                                                                                                                        
+ooooooooo.   oooooooooooo oooooo     oooo ooooo oooooooooooo oooooo   oooooo     oooo 
+`888   `Y88. `888'     `8  `888.     .8'  `888' `888'     `8  `888.    `888.     .8'  
+ 888   .d88'  888           `888.   .8'    888   888           `888.   .8888.   .8'   
+ 888ooo88P'   888oooo8       `888. .8'     888   888oooo8       `888  .8'`888. .8'    
+ 888`88b.     888    "        `888.8'      888   888    "        `888.8'  `888.8'     
+ 888  `88b.   888       o      `888'       888   888       o      `888'    `888'      
+o888o  o888o o888ooooood8       `8'       o888o o888ooooood8       `8'      `8'       
+                                                                                      
+                                                                                      
+[YOUR SIGNATURE HERE.]                                                                                      
+
+Author Name, Date, Time, AP CSA Topic 1.3 Review, v0.0
+*/
+public class TopicOnePointThreeReview {
+
+    public static void main(String[] args) {
+
+        /*
+         * Declare double variables named x and y. Assign x the value 1.0 Assign y the
+         * value 2.0
+         */
+
+        // Trace the next line in your journal BEFORE you run the code.
+        double z = ((x + y) / 1 - (y * 5) % 2);
+        System.out.println(z);
+        // Was your answer correct? If not, re-trace the code from scratch.
+
+    }
+}
